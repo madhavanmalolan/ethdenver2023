ee9c77aee9059f487bf07c78299cf636fed1f1d3
madhavanmalolan <madhavan93@gmail.com>
2023-02-25 17:15:01 -0700

    urlencoding

--- generatePage.py
+++ generatePage.py
@@ -23,11 +23,11 @@ for session in sessions:
   instance = session["instances"][0]
   event = {"name":session["name"], "description":session["description"].replace("\n"," "), "room":rooms[instance["room_id"]], "start_time": instance["time"], "end_time": instance["end_time"], "tz": instance["time_zone_offset"], "date": instance["date"]}
   google_url = "https://www.google.com/calendar/render?action=TEMPLATE&text="+event["name"]+"&dates="+event["date"].replace("-","")+"T"+event["start_time"].replace(":","")+"00Z"+event["tz"]+"/"+event["date"].replace("-","")+"T"+event["end_time"].replace(":","")+"00Z"+event["tz"]
-  event["google_url"] = google_url.replace(" ","%20")
+  event["google_url"] = google_url
   events.append(event)
 
 events.sort(key=lambda e : e["date"]+"T"+e["start_time"])
 
 for event in events:
-  fout.write("|"+event["name"]+"|"+event["description"]+"|"+event["room"]+"|"+event["start_time"]+"|"+event["date"]+"|[Add to Cal](\""+event["google_url"]+"\")|\n")
+  fout.write("|"+event["name"]+"|"+event["description"]+"|"+event["room"]+"|"+event["start_time"]+"|"+event["date"]+"|[Add to Cal](\""+event["google_url"].replace(" ","%20")+"\")|\n")
 
